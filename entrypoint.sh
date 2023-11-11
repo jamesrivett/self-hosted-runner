4fe437c3bda537fd82fd60f83f91a0205a4a8fe2
jamesrivett <jamesrivett77@gmail.com>
2023-11-11 00:00:24 -0500

    auto-detect org/repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,28 @@
 
 sudo chmod 666 /var/run/docker.sock
 
+if [ -n "$REPO" ]; then
+	REG_TOKEN=$(\
+		curl -L -X POST \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		https://api.github.com/repos/${USER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+	
+	GIT_URL=https://github.com/${USER}/${REPO}
+fi
+
+if [ -n "$ORG" ]; then
+	REG_TOKEN=$(\
+		curl -L -X POST \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer ${ACCESS_TOKEN}" \
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		https://api.github.com/orgs/${ORG}/actions/runners/registration-token | jq .token --raw-output)
+
+	GIT_URL=https://github.com/${ORG}
+fi
+
 /home/docker/actions-runner/config.sh \
 	--url ${GIT_URL} \
 	--token ${REG_TOKEN} \
